0846ac2a4179f491b6c71dec33d81be085144361
Tvv95 <truvladislav@gmail.com>
2020-08-12 15:31:00 +0300

    fix adaptive for mobile

--- src/Header/Header.js
+++ src/Header/Header.js
@@ -7,7 +7,7 @@ class Header extends React.Component {
   render() {
     return (
       <header className="main-header">
-        <a href="http://modzadrot.ru"><img src="logo.jpg" alt="MODZADROT" className="main-header-logo" /></a>
+        <a href="http://modzadrot.ru" target="_self"><img src="logo.jpg" alt="MODZADROT" className="main-header-logo" /></a>
         <div className="button-container">
           <a download href="http://modzadrot.ru/MZ_Launcher.exe">Скачать лаунчер</a>
         </div>
